b475d9fa45ef7e77f089e0c25cee3c42e22927b1
DenisPachekhon <denis.pachekhon.work@gmail.com>
2021-09-10 20:45:19 +0300

    The method Add has been changed (returns 0 for an empty string)

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,10 +1,18 @@
-﻿
+﻿using System;
+
 namespace StringCalculator
 {
     public class StringCalculator
     {
+        private const int SUM_FOR_AN_EMPTY_STRING = 0;
+
         public int Add(string numbers)
         {
+            if (String.IsNullOrEmpty(numbers))
+            {
+                return SUM_FOR_AN_EMPTY_STRING;
+            }
+
             return 1;
         }
     }
