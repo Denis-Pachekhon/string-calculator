7adce522b751a7cd0dd41a68b620f42f3a858a0c
DenisPachekhon <denis.pachekhon.work@gmail.com>
2021-09-10 19:48:46 +0300

    Creating a StringCalculator class. Create a test for an empty string

---
+++ StringCalculator.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringCalculator.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringCalculator.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4af3e2a5-0f21-4c9d-83db-8de862dd76b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculator.Tests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        private static StringCalculator calculator;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext textContext)
+        {
+            calculator = new StringCalculator();
+        }
+
+        [TestMethod]
+        public void Sum_EmptyString_Zero()
+        {
+            // arrange
+            var emptyString = String.Empty;
+            var expected = 0;
+
+            // act
+            var result = calculator.Add(emptyString);
+
+            // assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
