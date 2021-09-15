634a87ed47a14a5aa855b937218104ee6c850595
DenisPachekhon <denis.pachekhon.work@gmail.com>
2021-09-15 18:49:30 +0300

    Returns 0 for an empty or whire space string

--- StringCalculator.Tests/StringCalculatorXUnitTests.cs
+++ StringCalculator.Tests/StringCalculatorXUnitTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿
 using Xunit;
 
 namespace StringCalculator.Tests
@@ -14,171 +14,13 @@ namespace StringCalculator.Tests
 
         [Theory]
         [InlineData("", 0)]
-        [InlineData("\n", 0)]
+        [InlineData(" ", 0)]
         public void Add_EmptyOrWhiteSpace_Zero(string emptyString, int expected)
         {
             // arrange
 
             // act
             var result = calculator.Add(emptyString);
-
-            // assert
-            Assert.Equal(expected, result);
-        }
-
-        [Fact]
-        public void Add_OneNumber_ReturnsTheSameNumber()
-        {
-            // arrange
-            var oneNumber = "3";
-            var expected = 3;
-
-            // act
-            var result = calculator.Add(oneNumber);
-
-            // assert
-            Assert.Equal(expected, result);
-        }
-
-        [Fact]
-        public void Add_TwoNumber_ReturnsTheirSum()
-        {
-            // arrange
-            var twoNumber = "12,2";
-            var expected = 14;
-
-            // act
-            var result = calculator.Add(twoNumber);
-
-            // assert
-            Assert.Equal(expected, result);
-        }
-
-        [Fact]
-        public void Add_NewLine_NewLineAsSeparator()
-        {
-            // arrange
-            var newLine = "1,2\n3";
-            var expected = 6;
-
-            // act
-            var result = calculator.Add(newLine);
-
-            // assert
-            Assert.Equal(expected, result);
-        }
-
-        [Fact]
-        public void Add_DifferentDelimiter_SumNumbers()
-        {
-            // arrange
-            var differentDelimiter = "//;;;\n1;;;2";
-            var expected = 3;
-
-            // act
-            var result = calculator.Add(differentDelimiter);
-
-            // assert
-            Assert.Equal(expected, result);
-        }
-
-        [Fact]
-        public void Add_Negative_Throws()
-        {
-            // arrange
-            var negative = "-2";
-
-            // act
-
-            // assert
-            var actual = Assert.Throws<ApplicationException>(() => calculator.Add(negative));
-            Assert.Equal("Negatives not allowed: -2", actual.Message);
-        }
-
-        [Fact]
-        public void GetCalledCount_Call2Add_Called2Add()
-        {
-            // arrange
-            var expected = 2;
-
-            // act
-            calculator.Add("");
-            calculator.Add("");
-            var count = calculator.GetCalledCount();
-
-            // assert
-            Assert.Equal(expected, count);
-        }
-
-        [Fact]
-        public void Add_Event_EventCall()
-        {
-            // arrange
-            int giveResult = 0;
-
-            calculator.AddOccured += delegate (string input, int result)
-            {
-                giveResult = result;
-            };
-
-            // act
-            var sum = calculator.Add("1");
-
-            // assert
-            Assert.Equal(giveResult, sum);
-        }
-
-        [Fact]
-        public void Add_NumbersBiggerThan1000_Ignored()
-        {
-            // arrange
-            var numbersBiggerThan1000 = "1005, 3\n10002\n5";
-            var expected = 8;
-
-            // act
-            var result = calculator.Add(numbersBiggerThan1000);
-
-            // assert
-            Assert.Equal(expected, result);
-        }
-
-        [Fact]
-        public void Add_OtherFormatDelimiters_Supported()
-        {
-            // arrange
-            var otherFormatDelimiters = "//[***]\n1***2***3";
-            var expected = 6;
-
-            // act
-            var result = calculator.Add(otherFormatDelimiters);
-
-            // assert
-            Assert.Equal(expected, result);
-        }
-
-        [Fact]
-        public void Add_MultipleDelimiters_Supported()
-        {
-            // arrange
-            var multipleDelimiters = "//[*][%]\n1*2%3";
-            var expected = 6;
-
-            // act
-            var result = calculator.Add(multipleDelimiters);
-
-            // assert
-            Assert.Equal(expected, result);
-        }
-
-        [Fact]
-        public void Add_MultipleDelimitersWithLengthLongerThanOneChar_Supported()
-        {
-            // arrange
-            var multipleDelimiters = "//[**][%%]\n1**2%%3";
-            var expected = 6;
-
-            // act
-            var result = calculator.Add(multipleDelimiters);
 
             // assert
             Assert.Equal(expected, result);
--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,101 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace StringCalculator
 {
     public class StringCalculator
     {
-        private const int SUM_FOR_AN_EMPTY_STRING = 0;
-        private int Count = 0;
-        private const int MAX_NUMBER = 1000;
-        private List<string> SEPERATORS = new List<string>() { ",", "\n" };
-        private const string INDICATOR = "//";
-        public event Action<string, int> AddOccured;
+        private const int SUM_FOR_Null_Or_White_Space_STRING = 0;
 
         public int Add(string numbers)
         {
-            Count++;
-
             if (String.IsNullOrWhiteSpace(numbers))
             {
-                return SUM_FOR_AN_EMPTY_STRING;
+                return SUM_FOR_Null_Or_White_Space_STRING;
             }
 
-            if (numbers.StartsWith(INDICATOR))
-            {
-                AddNewSeperators(numbers);
-
-                numbers = ClearStringOfNumbersFromTheIndicators(numbers);
-            }
-
-            var listNumbers = GetNumbers(numbers);
-
-            int sum = listNumbers.Sum();
-
-            if (AddOccured != null)
-            { 
-                AddOccured.Invoke(numbers, sum);
-            }
-
-            return sum;
-        }
-            
-        private List<int> GetNumbers(string numbers)
-        {
-            var nums = numbers.Split(SEPERATORS.ToArray(), StringSplitOptions.RemoveEmptyEntries);
-
-            var listNumbers = new List<int>();
-
-            foreach (var num in nums)
-            {
-                var number = int.Parse(num);
-
-                if (number < 0)
-                {
-                    throw new ApplicationException("Negatives not allowed: " + GetNegativeNumbers(nums));
-                }
-
-                if (number <= MAX_NUMBER)
-                {
-                    listNumbers.Add(number);
-                }
-            }
-
-            return listNumbers;
-        }
-
-        private void AddNewSeperators(string numbers)
-        {
-            string[] indicators = { INDICATOR, "[", "]" };
-
-            var customSeperator = numbers.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries).First();
-
-            var seperators = customSeperator.Split(indicators, StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (var seperator in seperators)
-            {
-                SEPERATORS.Add(seperator);
-            }
-        }
-
-        private string ClearStringOfNumbersFromTheIndicators(string numbers)
-        {
-            var customSeperator = numbers.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries).First();
-
-            numbers = numbers.Substring(customSeperator.Length, numbers.Length - customSeperator.Length);
-
-            return numbers;
-        }
-
-        private string GetNegativeNumbers(string[] nums)
-        {
-            return String.Join(", ", nums.Where(x => int.Parse(x) < 0).Select(x => x.ToString()));
-        }
-
-        public int GetCalledCount()
-        {
-            return Count;
+            return 1;
         }
     }
 }
